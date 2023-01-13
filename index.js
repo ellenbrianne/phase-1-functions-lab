55d90262b3870f795a731cb7615c4a90216ac24f
Ellen Bennett <bennette831@gmail.com>
2023-01-12 19:18:49 -0700

    passing lab

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Code your solution in this file!
+const destination = 42;
+
+function distanceFromHqInBlocks(pickupLoc) {
+    let distanceBlock;
+    if (pickupLoc > destination) {
+    return distanceBlock = pickupLoc - destination;
+    } else if (pickupLoc < destination) {
+        return distanceBlock = destination - pickupLoc;
+    };
+    return distanceBlock;
+};
+
+function distanceFromHqInFeet(pickupLoc) {
+    let distanceFeet = distanceFromHqInBlocks(pickupLoc);
+
+    return distanceFeet *= 264;
+};
+
+function distanceTravelledInFeet(start, destination) {
+    let feetTravelled;
+    if (start > destination) {
+        return feetTravelled = (start - destination) * 264;
+    } else if (start < destination) {
+        return feetTravelled = (destination - start) * 264;
+    };
+
+    return feetTravelled;
+};
+
+function calculatesFarePrice(start, destination) {
+    let farePrice;
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+        return farePrice = 0;
+    } else if (distance > 400 && distance < 2000) {
+        return farePrice = (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance < 2500) {
+        return farePrice = 25;
+    } else if (distance > 2500) {
+        return farePrice = 'cannot travel that far';
+    };
+
+    return farePrice;
+};
